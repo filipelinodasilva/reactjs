7b01464fe549871dc86b689cc94fc1d88c1a85d3
filipelinodasilva <filipelinodasilva@hotmail.com>
2022-01-12 13:09:27 -0300

    Add Fast Refresh no Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: isDevelopment ? 'development' : 'production', 
+    mode: isDevelopment ? 'development' : 'production',
     devtool: isDevelopment ? 'eval-source-map' : 'source-map',
 
-    entry: path.resolve(__dirname, 'src', 'index.jsx'), 
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
 
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-      },
+    },
 
     resolve: {
         extensions: ['.js', '.jsx'],
@@ -20,24 +21,33 @@ module.exports = {
 
     devServer: {
         static: path.resolve(__dirname, 'public'),
-      },
+        hot: true,
+    },
 
     plugins: [
-        new HtmlWebpackPlugin({ 
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+        new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
+    ].filter(Boolean),
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader',
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                },
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
-                use: ['style-loader','css-loader','sass-loader'],
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ],
     }
