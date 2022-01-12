bcaad299822f012ee5c95a3e9a230a2b2a901293
filipelinodasilva <filipelinodasilva@hotmail.com>
2022-01-12 15:06:23 -0300

    Using useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,11 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
+// useEffect serve para disparar uma função quando algo(Variavel mudou, disparo de funcção  etc) acontecer na aplicação
+
 import '../styles/repositories.scss';
+
+// api.github.com/orgs/rocketseat/repos
 
 const repository = {
     name: 'unform',
@@ -9,6 +14,17 @@ const repository = {
 }
 
 export function RepositoryList() {
+    // Estado que Armazena uma Listagem de Repositorios
+    const [repositories, setRepositories] = useState([]);
+
+    // useEffect sempre vai precisar de dois parametros
+    // useEffect utilizado para chamada da API para requisição dos dados do GitHub da Rocketseat
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
 
