dd546db422b25d41235d126c0bce8e1d03cdd83c
filipelinodasilva <filipelinodasilva@hotmail.com>
2022-01-13 15:03:06 -0300

    TypeScript Fundamentals

---
+++ src/TypeScript.js
@@ -0,0 +1,21 @@
+type User = {
+    name: string
+    email: string
+    address: {
+        city: string
+        state?: string
+    }
+}
+
+function showWelcomeMessage(user: User) {
+    return `Welcome ${user.name}, your e-mail is ${user.email}. Your city is ${user.address.city} and your state is ${user.address.state} `;
+}
+
+showWelcomeMessage({
+    name: 'John Doe',
+    email: 'john@doe.com',
+    address: {
+        city:'New York',
+        state: 'NY',
+    }
+})
